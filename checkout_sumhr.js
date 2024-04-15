8f2ba864cb949fc5ab7d88868f9a7d2e0e4260ae
Soumit <soumitcse@yahoo.com>
2024-04-15 21:45:26 +0530

    added file to checkout fied at 8 PM

---
+++ checkout_sumhr.js
@@ -0,0 +1,18 @@
+function checkout(){
+  content= document.getElementsByClassName('clock-in-out')[0]
+  kbButtons = content.getElementsByTagName("button");
+ kbButtons[0].click();
+}
+
+current_date = new Date;
+start_date = "" + (current_date.getMonth()  + 1)+ " " + current_date.getDate() + " " + current_date.getFullYear()  + " " + current_date.getHours() + ":" + current_date.getMinutes()+":00"
+end_date = "" + (current_date.getMonth()  + 1)+ " " + current_date.getDate() + " " + current_date.getFullYear()  + "  21:00:00" 
+dt1 = new Date(start_date);
+dt2 = new Date(end_date);
+var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+ diff /= 60;
+timout_mini_sec = 1000  * 60 * Math.abs(Math.round(diff))
+setTimeout(() => {
+   checkout();
+}, timout_mini_sec);
+
