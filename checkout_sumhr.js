f2431bdedf308b07c63cefb9a0cfcb27169e4561
Soumit <soumitcse@yahoo.com>
2024-04-16 21:56:44 +0530

    Update checkout_sumhr.js

--- checkout_sumhr.js
+++ checkout_sumhr.js
@@ -1,12 +1,13 @@
 function checkout(){
   content= document.getElementsByClassName('clock-in-out')[0]
   kbButtons = content.getElementsByTagName("button");
- kbButtons[0].click();
+  kbButtons[0].click();
+  console.log("Checkout clicked");
 }
 
 current_date = new Date;
 start_date = "" + (current_date.getMonth()  + 1)+ " " + current_date.getDate() + " " + current_date.getFullYear()  + " " + current_date.getHours() + ":" + current_date.getMinutes()+":00"
-end_date = "" + (current_date.getMonth()  + 1)+ " " + current_date.getDate() + " " + current_date.getFullYear()  + "  21:00:00" 
+end_date = "" + (current_date.getMonth()  + 1)+ " " + current_date.getDate() + " " + current_date.getFullYear()  + "  19:00:00" 
 dt1 = new Date(start_date);
 dt2 = new Date(end_date);
 var diff =(dt2.getTime() - dt1.getTime()) / 1000;
